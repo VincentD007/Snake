164ce58cfc9edf563eaaa077b014a2ca6cd49162
Vincent <vincentedward007@gmail.com>
2024-08-17 20:01:50 -0700

    Added snake body parts that follow the head.

--- main.py
+++ main.py
@@ -7,48 +7,52 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
-VEL = 10
+VEL = 25
 
 
 def draw_window(snake, direction):
     screen.fill(BLACK)
+    for num in range(1, len(snake)):
+        snake[-num].x = snake[-num - 1].x
+        snake[-num].y = snake[-num - 1].y
+
+    if direction == pygame.K_UP:
+        snake[0].y -= VEL
+    elif direction == pygame.K_DOWN:
+        snake[0].y += VEL
+    elif direction == pygame.K_LEFT:
+        snake[0].x -= VEL
+    elif direction == pygame.K_RIGHT:
+        snake[0].x += VEL
     for part in snake:
-        if snake.index(part) == 0:
-            if direction == "up":
-                part.y -= VEL
-            elif direction == "down":
-                part.y += VEL
-            elif direction == "left":
-                part.x -= VEL
-            elif direction == "right":
-                part.x += VEL
-        pygame.draw.rect(screen, GREEN, snake[0])
+        pygame.draw.rect(screen, GREEN, part)
     pygame.display.update()
 
 
 def main():
-    snake = [pygame.Rect(HEIGHT/2 - 25, WIDTH/2 - 25, 50, 50)]
-    head_position_x, head_position_y = snake[0].x, snake[0].y
-    direction = ""
+    snake = [pygame.Rect(HEIGHT/2, WIDTH/2, 25, 25)]
+    for _ in range(5):
+        snake.append(pygame.Rect(HEIGHT/2, WIDTH/2, 25, 25))
+    last_input = None
     play_game = True
 
     while play_game:
-        clock.tick(10)
+        clock.tick(15)
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 play_game = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    direction = "up"
+                    last_input = pygame.K_UP
                 if event.key == pygame.K_DOWN:
-                    direction = "down"
+                    last_input = pygame.K_DOWN
                 if event.key == pygame.K_LEFT:
-                    direction = "left"
+                    last_input = pygame.K_LEFT
                 if event.key == pygame.K_RIGHT:
-                    direction = "right"
+                    last_input = pygame.K_RIGHT
 
-        draw_window(snake, direction)
+        draw_window(snake, last_input)
     pygame.quit()
 
 
